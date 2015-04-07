848a9827ca5063e97bb24f9c1b55f6d0ffcb54a2
phpPluginMaster <Q695@aol.com>
2015-04-07 18:09:17 -0500

    Create load_content.php

---
+++ load_content.php
@@ -0,0 +1,26 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<?php
+if (empty($pass_var)) {
+    $pass_var = '';
+}
+
+function div_swap($link_text, $div_target, $loader, $pass_var)
+{
+    global $link_text, $div_target, $loader, $pass_var, $i;
+    $i++;
+
+
+    echo "<a href='#' onclick='return false' onmousedown='javascript:swapContent$i($pass_var)'>$link_text</a>
+
+    <script language='javascript' type='text/javascript'>
+function swapContent$i(cv) {
+        $('#$div_target').html('Loading...').show();
+        var url = '$loader';
+        $.post(url, {contentVar: cv} ,function(data) {
+        $('#$div_target').html(data).show();
+        });
+    }
+    </script>
+";
+
+}
